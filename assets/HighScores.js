030aa5fc78ae8a034dc25b138aa0a49e1ca444fe
chdonovan <chrisdonovan311@gmail.com>
2021-04-16 09:12:46 -0500

    highscores html and js file uploaded logic complete

---
+++ assets/HighScores.js
@@ -0,0 +1,28 @@
+// variables
+
+var HighScore = document.querySelector("#highscore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#clear");
+
+// clear score event
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+});
+// local storage retrieval
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+if(allScores !==null){
+
+    for (var i = 0; i <allScores.length; i++){
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].intials + " " + allScores[i].score;
+        HighScore.appendChild(createLi);
+    }
+}
+
+// event to go back to index page
+goBack.addEventListener("click", function (){
+    window.location.replace("./index.html");
+});
